d8288530619b0735ebd9543c383d6c99cd80b4dc
Valerij Dobler <vd@wps.de>
2022-06-30 22:39:30 +0200

    Adds customizable mediatype

--- src/main/java/tech/dobler/cmsbenchmarkingapp/Controller.java
+++ src/main/java/tech/dobler/cmsbenchmarkingapp/Controller.java
@@ -3,6 +3,9 @@ package tech.dobler.cmsbenchmarkingapp;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -27,9 +30,12 @@ public class Controller {
     @Value("${response:hello world}")
     private String defaultResponse;
 
+    @Value("${responseType:text/plain}")
+    private String responseType;
+
 
     @GetMapping
-    public String get() throws InterruptedException {
+    public ResponseEntity<String> get() throws InterruptedException {
         LOGGER.debug("Delaying get request for {}ms", delay);
         Thread.sleep(delay);
         int countForThisCall = getCounter.incrementAndGet();
@@ -38,11 +44,12 @@ public class Controller {
         if (appendAmountOfCallsToResponse) {
             response += countForThisCall;
         }
-        return response;
+
+        return wrapResponseWithMediaType(response);
     }
 
     @PostMapping
-    public String post(@RequestParam String param) throws InterruptedException {
+    public ResponseEntity<String> post(@RequestParam String param) throws InterruptedException {
         LOGGER.debug("Delaying post request for {}ms", delay);
         Thread.sleep(delay);
         int countForThisCall = postCounter.incrementAndGet();
@@ -54,6 +61,31 @@ public class Controller {
         if (appendParamToResponse) {
             response += param;
         }
-        return response;
+
+        return wrapResponseWithMediaType(response);
+    }
+
+    private ResponseEntity<String> wrapResponseWithMediaType(String response) {
+        final HttpHeaders httpHeaders = new HttpHeaders();
+        final MediaType mediaType;
+        switch (responseType) {
+            case "json":
+                mediaType = MediaType.APPLICATION_JSON;
+                break;
+            case "plain":
+            case "text":
+                mediaType = MediaType.TEXT_PLAIN;
+                break;
+            default:
+                final var parts = responseType.split("/");
+                if (parts.length < 2)
+                    throw new IllegalArgumentException(String.format("Unrecognizable MediaType '%s'", responseType));
+                mediaType = new MediaType(parts[0], parts[1]);
+                break;
+        }
+        LOGGER.debug("Using mediaType {}", mediaType);
+        httpHeaders.setContentType(mediaType);
+
+        return ResponseEntity.ok().headers(httpHeaders).body(response);
     }
 }
